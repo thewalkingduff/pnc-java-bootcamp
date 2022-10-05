75fae52cc0e35fe5b9eda2589cc5d7e89607bf24
Anjaom <eric@webworkbench.net>
2022-10-05 09:37:24 -0600

    coffee shop user input

--- class-examples/src/main/java/com/teksystems/coffee/MainMenu.java
+++ class-examples/src/main/java/com/teksystems/coffee/MainMenu.java
@@ -1,6 +1,7 @@
 package com.teksystems.coffee;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class MainMenu {
 
@@ -9,8 +10,8 @@ public class MainMenu {
 	// should use a while loop to keep displaying the menu until the user selects
 	// exit
 	// print a menu 
-	// 1) order item 2) fulfillOrder 3) List menu 4) print total 5) list drinks 6) list food 7) exit
-	
+	// 1) order item 2) list order 3) fulfillOrder 4) List menu 5) print total 6) list drinks 7) list food 8) exit
+	// 
 	// -- 1 function that displays the menu, collects the user input, and returns an integer for selection
 	//  if the user type 1 and hits enter this function will return 1
 	
@@ -21,16 +22,85 @@ public class MainMenu {
 	// then if a user select 4 print to total and go back to the main menu
 	// 
 	// BONUS dont forget some error checking ... if a user enters a letter instead of a number gracefully handle that
+	// BONUS remove an item from the order.
 	
+	private Scanner scanner = new Scanner(System.in);
+	private CoffeeShop shop = new CoffeeShop();
 	
-
 	public static void main(String[] args) {
 		MainMenu m = new MainMenu();
 		m.start();
 	}
-
-
+	
+	// TODO change this method to reduce complexity in the while loop condition
 	public void start() {
+		int input = showMenuAndCaptureUserInput();
+		while( input != 4 ) {
+			switch ( input ) {
+			case 1 :
+				// order item
+				orderMenuItem();
+				break;
+			case 2 :
+				// list order
+				listOrderDetails();
+				break;
+			case 3 : 
+				// fulfill order
+				break;
+			case 4 :
+				// exit program
+				System.exit(0);
+			default :
+				// this is the catch all and is an error state
+				// for example the user enters a non valid menu selection
+				System.out.println("Invalid menu selection");
+			}
+			
+			input = showMenuAndCaptureUserInput();
+		}
+	}
+	
+	// TODO fix this function to display a different message if there are no items ordered
+	// TODO use the formatter to make the amount due print nicely with a $
+	private void listOrderDetails() {
+		String[] orderItems = shop.listOrders();
+		
+		System.out.println("========================");
+		System.out.println("Your current order contains:");
+		
+		for ( String item : orderItems ) {
+			System.out.println(item);
+		}
+		
+		System.out.println("\nTotal Amount Due: " + shop.dueAmount());
+	}
+	
+	private void orderMenuItem() {
+		System.out.print("Enter item to order ");
+		// remember that if you are capturing spaces from the user input to use nextLine
+		String itemName = scanner.nextLine();
+		
+		String success = shop.addOrder(itemName);
+		System.out.println("\n" + success + "\n");
+	}
+	
+	private int showMenuAndCaptureUserInput() {
+		System.out.println("========================");
+		System.out.println("1) Order Item");
+		System.out.println("2) List Order");
+		System.out.println("3) Fulfill Order");
+		System.out.println("4) Exit");
+		
+		System.out.print("Enter menu option ");
+		
+		int input = scanner.nextInt();
+		scanner.nextLine();
+		return input;
+	}
+
+
+	public void startBasic() {
 		// while loop
 		
 		
