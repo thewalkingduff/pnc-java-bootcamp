95547ce96eb88456b1cde23eb2d1448abfdc431d
Anjaom <eric@webworkbench.net>
2022-10-27 14:13:46 -0600

    removed old project

--- database/src/main/java/database/entity/Order.java
+++
@@ -1,9 +0,0 @@
-package database.entity;
-
-import java.util.List;
-
-public class Order {
-	
-	// the list of products associted with this order - based on join of orderNumber column
-	private List<OrderDetail> orderDetails;
-}
